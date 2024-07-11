2d6e624a1679f0a36e773db7a0787863ea371ad5
Manikandan Anbalagan <manikandana121290@gmail.com>
2024-07-11 21:36:16 +0530

    node day - 2

--- Node-Day-1/client/src/App.jsx
+++ Node-Day-1/client/src/App.jsx
@@ -14,21 +14,21 @@ function App() {
       })
   }, []);
 
+  const getGreet = () => {
+    axios.get('http://localhost:3000/greet')
+      .then(res => {
+        setGreeting(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
   return (
     <div>
       <header>
         <h1>{greeting}</h1>
-        <button onClick={() => {
-          axios.get('http://localhost:3000/greet')
-            .then(res => {
-              setGreeting(res.data);
-            })
-            .catch(err => {
-              console.log(err);
-            })
-        }}>
-          Get Greeting
-        </button>
+        <button onClick={getGreet}>Click Me</button>
       </header>
     </div>
   )
